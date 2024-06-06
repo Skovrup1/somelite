7aa24420af8108a239d46816f725be10e675da6a
Skovrup1 <40407100+Skovrup1@users.noreply.github.com>
2024-06-06 15:29:54 +0200

    quick & dirty, user page

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from database import Db
+from user import User
 
 db = Db("somelite", "somelite", "postgres", "postgres")
+current_user = User(1, "Alice")
+
 
 def create_app():
     app = Flask(__name__)
@@ -9,14 +12,17 @@ def create_app():
     app.config["SECRET_KEY"] = "password"
 
     # delete the old db if it exists
+    print("recreating database")
     db.delete()
     db.create()
     db.create_tables()
 
     from auth import auth
+
     app.register_blueprint(auth)
 
     from main import main
+
     app.register_blueprint(main)
 
     return app
